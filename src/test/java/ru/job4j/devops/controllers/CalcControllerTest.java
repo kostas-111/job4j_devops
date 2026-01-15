f1e83461d10343e94d92928489eff8a0c2f58981
galkinki <galkin@safe-line.ru>
2026-01-15 13:00:37 +0300

    feat: add test

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -62,4 +62,13 @@ class CalcControllerTest {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
+
+    @Test
+    public void whenNegativeNumberSecondTest() {
+        var input = new TwoArgs(-1, -2);
+        var expected = new Result(-3);
+        var output = new CalcController().summarise(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
+    }
 }
