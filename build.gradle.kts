af4a229df346e8f88fc50a510cbcb7809707648a
galkinki <galkin@safe-line.ru>
2026-01-26 18:22:25 +0300

    feat: Настройки окружения | dotEnv [#505367]

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
     id("com.github.spotbugs") version "6.0.26"
     id("org.liquibase.gradle") version "3.0.1"
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
 }
 
 group = "ru.job4j.devops"
@@ -91,6 +92,12 @@ tasks.register<Zip>("archiveResources") {
     }
 }
 
+tasks.register("profile") {
+    doFirst {
+        println(env.DB_URL.value)
+    }
+}
+
 tasks.check {
     dependsOn("jacocoTestCoverageVerification", "checkJarSize")
 }
@@ -148,9 +155,9 @@ liquibase {
     activities.register("main") {
         this.arguments = mapOf(
             "logLevel"       to "info",
-            "url"            to "jdbc:postgresql://localhost:5432/job4j_devops",
-            "username"       to "postgres",
-            "password"       to "postgres",
+            "url"            to env.DB_URL.value,
+            "username"       to env.DB_USERNAME.value,
+            "password"       to env.DB_PASSWORD.value,
             "classpath"      to "src/main/resources",
             "changelogFile"  to "db/changelog/db.changelog-master.xml"
         )
